5850ca793e994110167bf35a51e27f292f8900a2
johansson017 <oliver.johansson.jkp@gmail.com>
2021-12-03 00:05:17 +0100

    day1&2

---
+++ Day_1.py
@@ -0,0 +1,32 @@
+"""
+Day1 of AdventofCode
+"""
+import numpy as np
+
+with open("sonar_data.txt") as file:
+    f = file.read()
+    sonar = list(map(int, f.split("\n")))
+
+# part 1
+increases = 0
+prev = 0
+for ind, val in enumerate(sonar):
+    if ind == 0:
+        continue
+    elif val > prev:
+        increases += 1
+    prev = val
+
+print(increases)
+
+# part 2
+increases = 0
+vals = []
+for ind, val in enumerate(sonar):
+    vals.append(val)
+    if len(vals) == 4:
+        if np.sum(vals[-3:]) > np.sum(vals[0:3]):
+            increases += 1
+        vals.remove(vals[0])
+
+print(increases)
---
+++ Day_2.py
@@ -0,0 +1,40 @@
+"""
+Day 2 of AdventofCode
+"""
+
+with open("day2_data.txt") as file:
+    f = file.read()
+    temp = f.split("\n")
+
+data = []
+for d in temp:
+    t = d.split(" ")
+    data.append([t[0], int(t[1])])
+
+depth = 0
+hori = 0
+for direc, val in data:
+    if direc == "forward":
+        hori += val
+    elif direc == "down":
+        depth += val
+    elif direc == "up":
+        depth -= val
+
+print(f"total value for part 1 is: {hori*depth}")
+
+hori = 0
+depth = 0
+aim = 0
+for direc, val in data:
+    if direc == "forward":
+        hori += val
+        depth += val*aim
+    elif direc == "down":
+        aim += val
+    elif direc == "up":
+        aim -= val
+
+print(f"total value for part 2 is: {hori*depth}")
+
+
