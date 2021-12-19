2be7d8cf2db1ae9bbf5101c747f1b280e289ff2a
johansson017 <oliver.johansson.jkp@gmail.com>
2021-12-19 18:56:11 +0100

    day15

--- Day_15.py
+++ Day_15.py
@@ -1,85 +1,90 @@
 """
 Day 15 Advent of Code
 """
+from queue import PriorityQueue
 import numpy as np
 
 def data(file):
     arrays = [x for x in open(file).read().split("\n")]
     x = len(arrays[0])
     y = len(arrays)
-    return np.array([int(x) for x in [x for x in open(file).read().replace("\n","")]]).reshape(x,y)
+    grid = np.array([int(x) for x in [x for x in open(file).read().replace("\n","")]]).reshape(x,y)
+    valgrid = dict()
+    prevgrid = dict()
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            valgrid[(i,j)] = np.inf
+            prevgrid[(i,j)] = list()
 
+    return grid, valgrid, prevgrid
 
-def part(grid, x, y):
+def part2grid(grid):
+    grid = np.concatenate((grid, grid+1, grid + 2, grid + 3, grid + 4), axis = 1)
+    grid = np.concatenate((grid, grid+1, grid+2, grid+3, grid+4))
+    grid = grid % 9
+    ind = grid == 0
+    grid[ind] = 9
+    
+    valgrid = dict()
+    prevgrid = dict()
+    for i in range(len(grid)):
+        for j in range(len(grid[0])):
+            valgrid[(i,j)] = np.inf
+            prevgrid[(i,j)] = list()
+
+    return grid, valgrid, prevgrid
+
+def pathing(grid, valgrid, prevgrid, start, end):
     dx = [0,0,-1,1]
     dy = [1,-1,0,0]
+    
+    valgrid[(0,0)] = 0
 
-    undiscovered = [(y,x)]
-    # grid(sy,sx) -> row(y),col(x)
-    visited = []
-    gtemp = 10000
+    qSet = PriorityQueue()
+    qSet.put((valgrid[0,0], (0,0)))
 
-    score = 0
-    while undiscovered:
-        if x == 9 and y == 9:
+    while not qSet.empty():
+        
+        u = qSet.get()
+        
+        y,x = u[1]
+
+        if u == end:
             break
-
-        y,x = undiscovered.pop()
-
-        visited.append((y,x))
-        score += grid[(y,x)]
-
+        
         for i in range(len(dx)):
             ny = y + dy[i]
             nx = x + dx[i]
-            if nx < 0 or ny < 0 or nx > 9 or ny > 9 or (ny,nx) in visited:
+
+            if nx < 0 or ny < 0 or nx > end[1] or ny > end[0]:
                 continue
             
-            temp = score + grid[(ny,nx)]
-            if temp < gtemp:
-                gtemp = temp
-                best = (ny,nx)
+            temp = valgrid[(y,x)] + grid[(ny,nx)]
+            if temp < valgrid[(ny,nx)]:
+                qSet.put((valgrid[(ny,nx)], (ny,nx)))
+                valgrid[(ny,nx)] = temp
 
-        y,x = best
-        undiscovered.append((y,x))
-        
-
-
-        
-
-
-def recursive(grid, y, x):
-    dx = [0,0,-1,1]
-    dy = [1,-1,0,0]
-
-    visited = []
-    visited.append((y,x))
-
-    score = 0
-    score += grid[(y,x)]
-
-    if x == 9 and y == 9:
-        return score
-
-    for i in range(len(dx)):
-        ny = y + dy[i]
-        nx = x + dx[i]
-
-        if nx < 0 or ny < 0 or nx > 9 or ny > 9 or (ny,nx) in visited:
-            continue
-
-        sum = recursive(grid, ny, nx, visited.copy(), score.copy())
-        if sum != None:
-            scores.append(sum)
-
-
-
+    return valgrid[(end)]
 
 if __name__ == "__main__":
+    """ Part 1 """
     scores = list()
-    grid = data("input_data/day15_testdata.txt")
-    print(grid)
-    part(grid, 0, 0)
+    grid, valgrid, prevgrid = data("input_data/day15_data.txt")
+    rowmax = len(grid)-1
+    colmax = len(grid[0])-1
+    end = (rowmax,colmax)
+    start = (0,0)
+    score1 = pathing(grid, valgrid, prevgrid, start, end)
+
+    """ Part 2 """
+    grid, valgrid, prevgrid = part2grid(grid)
+    rowmax = len(grid)-1
+    colmax = len(grid[0])-1
+    end = (rowmax,colmax)
+    start = (0,0)
+    score2 = pathing(grid, valgrid, prevgrid, start, end)
+
+    print(f"Part 1 Solution: {score1}\nPart 2 Solution: {score2}")
 
 
 
